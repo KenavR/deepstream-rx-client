35d5816bedfa4eda7be1f5890b7a70067c33a311
KenavR <kenavr@gmail.com>
2016-02-24 19:19:51 +0100

    Changed to common js dist files

--- dist/List.js
+++ dist/List.js
@@ -1,34 +1,23 @@
-System.register(["./Record"], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
-    var Record_1;
-    var List;
-    return {
-        setters:[
-            function (Record_1_1) {
-                Record_1 = Record_1_1;
-            }],
-        execute: function() {
-            List = (function () {
-                function List(deepstream) {
-                    this._deepstream = deepstream;
-                    this._record = new Record_1.default(deepstream);
-                }
-                List.prototype.isEmpty = function () {
-                };
-                List.prototype.getEntries = function () {
-                };
-                List.prototype.setEntries = function (entries) {
-                };
-                List.prototype.addEntry = function (entry, index) {
-                };
-                List.prototype.discard = function () {
-                };
-                List.prototype.delete = function () {
-                };
-                return List;
-            }());
-            exports_1("default", List);
-        }
+"use strict";
+var Record_1 = require("./Record");
+var List = (function () {
+    function List(deepstream) {
+        this._deepstream = deepstream;
+        this._record = new Record_1.default(deepstream);
     }
-});
+    List.prototype.isEmpty = function () {
+    };
+    List.prototype.getEntries = function () {
+    };
+    List.prototype.setEntries = function (entries) {
+    };
+    List.prototype.addEntry = function (entry, index) {
+    };
+    List.prototype.discard = function () {
+    };
+    List.prototype.delete = function () {
+    };
+    return List;
+}());
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = List;
--- dist/Record.js
+++ dist/Record.js
@@ -1,46 +1,35 @@
-System.register(["rxjs/Rx"], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
-    var Rx_1;
-    var Record;
-    return {
-        setters:[
-            function (Rx_1_1) {
-                Rx_1 = Rx_1_1;
-            }],
-        execute: function() {
-            Record = (function () {
-                function Record(deepstream) {
-                    this._deepstream = deepstream;
-                }
-                Record.prototype.getList = function (name) {
-                    var dsList = this._deepstream.record.getList(name);
-                    return Rx_1.Observable.fromEvent(dsList, "ready");
-                };
-                Record.prototype.getListWithEntries = function (name) {
-                    var _this = this;
-                    return this.getList(name)
-                        .flatMap(function (id) { return _this.getRecord(id); })
-                        .map(function (record) { return record.get(); });
-                };
-                Record.prototype.getRecord = function (name) {
-                    return Rx_1.Observable.from(this._deepstream.getRecord(name));
-                };
-                Record.prototype.get = function (path) {
-                };
-                Record.prototype.set = function (path, value) {
-                };
-                Record.prototype.subscribe = function () {
-                };
-                Record.prototype.unsubscribe = function () {
-                };
-                Record.prototype.discard = function () {
-                };
-                Record.prototype.delete = function () {
-                };
-                return Record;
-            }());
-            exports_1("default", Record);
-        }
+"use strict";
+var Rx_1 = require("rxjs/Rx");
+var Record = (function () {
+    function Record(deepstream) {
+        this._deepstream = deepstream;
     }
-});
+    Record.prototype.getList = function (name) {
+        var dsList = this._deepstream.record.getList(name);
+        return Rx_1.Observable.fromEvent(dsList, "ready");
+    };
+    Record.prototype.getListWithEntries = function (name) {
+        var _this = this;
+        return this.getList(name)
+            .flatMap(function (id) { return _this.getRecord(id); })
+            .map(function (record) { return record.get(); });
+    };
+    Record.prototype.getRecord = function (name) {
+        return Rx_1.Observable.from(this._deepstream.getRecord(name));
+    };
+    Record.prototype.get = function (path) {
+    };
+    Record.prototype.set = function (path, value) {
+    };
+    Record.prototype.subscribe = function () {
+    };
+    Record.prototype.unsubscribe = function () {
+    };
+    Record.prototype.discard = function () {
+    };
+    Record.prototype.delete = function () {
+    };
+    return Record;
+}());
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Record;
